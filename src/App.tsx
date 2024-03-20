25bc4c1d3a9a58beb2078f38a96d1759dfbd7fcb
avinashkumar002 <avinashsingh30oct@gmail.com>
2024-03-20 18:03:49 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+const Customers = lazy(() => import("./pages/Customers"));
+const NewProduct = lazy(() => import("./pages/management/NewProduct"));
+const ProductManagement = lazy(
+  () => import("./pages/management/ProductManagement")
+);
+const TransactionManagement = lazy(
+  () => import("./pages/management/TransactionManagement")
+);
+
+const BarCharts = lazy(() => import("./pages/charts/BarCharts"));
+const LineCharts = lazy(() => import("./pages/charts/LineCharts"));
+const PieCharts = lazy(() => import("./pages/charts/PieCharts"));
+
+const Stopwatch = lazy(() => import("./pages/apps/Stopwatch"));
+const Coupon = lazy(() => import("./pages/apps/Coupon"));
+const Toss = lazy(() => import("./pages/apps/Toss"));
+
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+        <Route
+  path="/"
+  element={
+    <div className="bg">
+      <h1 className="h">Welcome to the admin panel....</h1>
+      <h3 className="ht">This is a web-based interface that allows administrators to manage and control a system or application. It enables tasks such as user management, content management, data management, configuration, analytics, security, and communication, facilitating efficient oversight and maintenance of the system.</h3>
+      <Link to="/admin/dashboard">
+        <button className="btn">Visit Dashboard</button>
+      </Link>
+    </div>
+  }
+/>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/product" element={<Products />} />
+          <Route path="/admin/customer" element={<Customers />} />
+          <Route path="/admin/transaction" element={<Transaction />} />
+
+          {/* Charts */}
+
+          <Route path="/admin/chart/bar" element={<BarCharts />} />
+          <Route path="/admin/chart/pie" element={<PieCharts />} />
+          <Route path="/admin/chart/line" element={<LineCharts />} />
+
+          {/* Apps */}
+
+          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          <Route path="/admin/app/coupon" element={<Coupon />} />
+          <Route path="/admin/app/toss" element={<Toss />} />
+
+          {/* Management */}
+          <Route path="/admin/product/new" element={<NewProduct />} />
+          <Route path="/admin/product/:id" element={<ProductManagement />} />
+          <Route
+            path="/admin/transaction/:id"
+            element={<TransactionManagement />}
+          />
+        </Routes>
+      </Suspense>
+    </Router>
+  );
+};
+
+export default App;
